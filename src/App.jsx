8e51790957a26bd86a35ebcbf0dad654c252c2ff
zubdotexe <64923600+zubdotexe@users.noreply.github.com>
2025-09-23 21:01:55 +0600

    implemented section toggling

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Banner from "./components/Banner/Banner";
 import AvailablePlayers from "./components/AvailablePlayers/AvailablePlayers";
+import { useState } from "react";
 
 const res = await fetch("/playersData.json");
 const playersJson = await res.json();
-console.log(playersJson);
 
 function App() {
+    const [availablePlayers, setAvailablePlayers] = useState(true);
+    console.log(availablePlayers);
     return (
         <>
             <header className="space-y-6">
@@ -17,14 +19,35 @@ function App() {
             <main className="max-w-[1320px] mx-auto mt-24 space-y-9">
                 <div className="flex justify-between">
                     <h2 className="font-bold text-2xl">Available Players</h2>
-                    <div className="flex items-center rounded-2xl overflow-hidden border-1 border-gray-300">
-                        <p className="bg-[#E7FE29] font-bold px-6 py-3 cursor-pointer">Available</p>
-                        <p className="text-gray-400 px-6 py-3 cursor-pointer">
+                    <div className="flex items-center rounded-2xl overflow-hidden border border-gray-300">
+                        <p
+                            onClick={() => setAvailablePlayers(true)}
+                            className={`${
+                                availablePlayers ?
+                                "bg-[#E7FE29] font-bold text-black" : "text-gray-500 font-semibold"
+                            } px-6 py-3 cursor-pointer`}
+                        >
+                            Available
+                        </p>
+                        <p
+                            onClick={() => setAvailablePlayers(false)}
+                            className={`${
+                                !availablePlayers ?
+                                "bg-[#E7FE29] font-bold text-black" : "text-gray-500 font-semibold"
+                            } px-6 py-3 cursor-pointer`}
+                        >
                             Selected (<span>0</span>)
                         </p>
                     </div>
                 </div>
-                <AvailablePlayers playersJson={playersJson}></AvailablePlayers>
+                {availablePlayers ? (
+                    <AvailablePlayers
+                        playersJson={playersJson}
+                    ></AvailablePlayers>
+                ) : (
+                    <h3>Selected Players</h3>
+                )}
+                {/* <h3>Selected Players</h3> */}
             </main>
         </>
     );
